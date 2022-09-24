8b6cc51415460624de1c946ab7a91c8fe0314d6f
Saqlain <iamkhatik786@gmail.com>
2022-09-24 15:46:28 +0530

    large

--- HashTable.java
+++ HashTable.java
@@ -4,10 +4,11 @@ public class HashTable {
 
 	public static void main(String[] args) {
 		System.out.println("Hash table API!!!");
-		String testString = "To be or not to be";
-		
-		HashMap<String, Integer> hashMap = new HashMap<>();
-		String[] words = testString.toLowerCase().split(" ");
+		String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+		LinkedHashMap<String, Integer> hashMap = new LinkedHashMap<>();
+		String[] words = paragraph.toLowerCase().split(" ");
+
 		for (String word : words) {
 			Integer value = hashMap.get(word);
 			if (value == null)
@@ -16,7 +17,8 @@ public class HashTable {
 				value = value + 1;
 			hashMap.add(word, value);
 		}
-		
+
+		System.out.println(hashMap);
 		for (String word : words) {
 			int frequency = hashMap.get(word);
 			System.out.println("Frequency of '" + word + "' : \t" + frequency);
---
+++ LinkedHashMap.java
@@ -0,0 +1,56 @@
+package com.bl.hashtables;
+
+import java.util.ArrayList;
+
+public class LinkedHashMap<K,V> {
+
+	private final int numberOfBuckets;
+	ArrayList<LinkedList<K>> bucketArray;
+	
+	public LinkedHashMap() {
+		this.numberOfBuckets = 10;
+		this.bucketArray = new ArrayList<LinkedList<K>>(numberOfBuckets);
+		
+		for(int index = 0; index < numberOfBuckets; index++)
+			this.bucketArray.add(null);
+	}
+	
+	public void add(K key, V value) {
+		int index = this.getBucketIndex(key);
+		LinkedList<K> linkedList = this.bucketArray.get(index);
+
+		if(linkedList == null) {
+			linkedList = new LinkedList<K>();
+			this.bucketArray.set(index, linkedList);
+		}
+		
+		MyMapNode<K, V> mapNode = (MyMapNode<K, V>) linkedList.search(key);
+		if(mapNode == null) {
+			mapNode = new MyMapNode<>(key, value);
+			linkedList.append(mapNode);
+		}
+		else
+			mapNode.setValue(value);
+	}	
+	
+	public V get(K key) {
+		int index = this.getBucketIndex(key);
+		LinkedList<K> linkedList = this.bucketArray.get(index);
+		if (linkedList == null)
+			return null;
+		MyMapNode<K,V> mapnode = (MyMapNode<K,V>)linkedList.search(key);
+		return (mapnode == null) ? null : mapnode.getValue();
+	}
+
+	@Override
+	public String toString() {
+		return "LinkedHashMap { " + bucketArray + "}";
+	}
+
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numberOfBuckets;
+		//System.out.println("Key : \t'" + key + "' \tHashcode : " + hashCode + " \tindex : " + index);
+		return index;
+	}
+}
