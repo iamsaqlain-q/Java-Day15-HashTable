95957c66e7cd213e671539b54047f8f7b43b07f0
Saqlain <iamkhatik786@gmail.com>
2022-09-24 15:29:27 +0530

    Ability to find frequency of words

---
+++ Node.java
@@ -0,0 +1,37 @@
+package com.bl.hashtables;
+
+public class Node<K> implements INode<K> {
+
+	private K key;
+	private INode<K> next;
+
+	public Node(K key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public Node() {
+
+	}
+
+	@Override
+	public K getKey() {
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	@Override
+	public INode<K> getnext() {
+
+		return null;
+	}
+
+	@Override
+	public void setnext(INode<K> next) {
+		this.next = next;
+	}
+}
